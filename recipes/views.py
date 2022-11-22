77eaa39bfa3f628121244d113e3cc89937d9a008
Edu <dureisal1994@gmail.com>
2022-11-22 18:13:52 -0300

    add faker to fill recipes with random info

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Eduardo',
+        'page': 'Home | Recipes',
+        'recipes': [make_recipe() for _ in range(10)],
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe.html', context={
+        'page': 'Recipe | Recipes',
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
