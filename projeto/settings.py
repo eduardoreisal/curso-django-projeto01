cdc5c7cc3ddc90091d45a00ec78d86831e11c854
Edu <dureisal1994@gmail.com>
2022-11-22 10:25:34 -0300

    add static files. Tur css into a static file

--- projeto/settings.py
+++ projeto/settings.py
@@ -32,12 +32,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "django.contrib.admin",
-    "recipes",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # MY Apps
+    "recipes",
 ]
 
 MIDDLEWARE = [
