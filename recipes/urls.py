91859de8be766b5bead66fa81af78c4fc096000e
Edu <dureisal1994@gmail.com>
2022-11-22 19:14:49 -0300

    Dynamic names for urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path("", views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path("", views.home, name="recipes-home"),
+    path('recipes/<int:id>/', views.recipe, name="recipes-recipe"),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>', views.recipe, name="recipe"),
 ]
