7002640f560a7c76d2e4cec8563acc20ec396e31
Edu <dureisal1994@gmail.com>
2022-11-24 18:34:55 -0300

    get recipe from database e show them on view

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,6 +6,8 @@ app_name = 'recipes'
 
 urlpatterns = [
     path("", views.home, name="recipes-home"),
+    path('recipes/category/<int:category_id>/',
+         views.category, name="category"),
     path('recipes/<int:id>/', views.recipe, name="recipes-recipe"),
     path('', views.home, name="home"),
     path('recipes/<int:id>', views.recipe, name="recipe"),
--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 
+from recipes.models import Recipe
 from utils.recipes.factory import make_recipe
 
 
 # Create your views here.
 def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'page': 'Home | Recipes',
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+        # 'recipes': [make_recipe() for _ in range(10)],
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True
+    ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'page': 'Category | Recipes',
+        'recipes': recipes,
+        # 'recipes': [make_recipe() for _ in range(10)],
     })
 
 
